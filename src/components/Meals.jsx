87df1ebddfc68081fe43bd8d3f87d85f4551416c
Marcelle Pernet <mevpernet@gmail.com>
2022-10-27 10:36:56 -0300

    context

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,11 @@
+import { useContext } from 'react'
+import { AppContext } from '../../context'
+
 const Meals = () => {
+
+    const context = useContext(AppContext)
+    console.log(context)
+
     return (
         <h1>Meals</h1>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { AppProvider } from '../context'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 )
